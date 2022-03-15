b9eea47722333e34d4042e41121fdc216416986e
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-03-15 23:53:03 +0100

    API call on returned json object to get elite spec info. Extracted css styles

--- public/get_character_info.js
+++ public/get_character_info.js
@@ -1,40 +1,100 @@
 const get_character_info = (character_name) => {
-  fetch(`https://api.guildwars2.com/v2/characters/${character_name}?access_token=${access_token}`).then((response) => {
-    return response.json();
+  var character_info, character_spec;
+  fetch(`https://api.guildwars2.com/v2/characters/${character_name}?access_token=${access_token}`)
+  .then((characters) => {
+    return characters.json();
   })
   .then((character) => {
+    character_info = character;
+    return fetch(`https://api.guildwars2.com/v2/specializations/${character.specializations.pve[2].id}`);
+  })
+  .then((spec_info) => {
+    return spec_info.json();
+  })
+  .then((elite_spec) => {
+    elite_spec_pic = elite_spec.profession_icon_big;
+    elite_spec_name = elite_spec.name;
+
     characterBox = document.createElement('div');
     characterBox.className = "characterBox";
-    characterName = document.createElement('p');
-    characterName.className = "characterName";
-    characterName.innerText = `${character.name}`;
 
-    // characterBirthdate = document.createElement('p');
-    // characterBirthdate.className = "characterBirthdate";
-    // characterBirthdate.innerText = `${character.created.substring(0, 10)}`;
+    header = document.createElement('header');
+    header.className = "characterName";
+    characterName = `${character_info.name}`;
+
+    article = document.createElement('article');
+    article.className = "main";
+
+    aside_1 = document.createElement('aside');
+    aside_1.className = "aside aside_1";
+
+    aside_2 = document.createElement('aside');
+    aside_2.className = "aside aside_2";
+    
+    footer = document.createElement('footer');
+    footer.className = "footer";
+
+    characterBirthdate = document.createElement('span');
+    characterBirthdate.className = "characterBirthdate";
+    var char_creation = new Date(character_info.created);
+    var creation_dd = String(char_creation.getDate()).padStart(2, '0');
+    var creation_mm = String(char_creation.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var creation_yyyy = char_creation.getFullYear();
+    var creation_string = new String(creation_yyyy + '-' + creation_mm + '-' + creation_dd);
+    var creation_final = creation_string.split('-');
+
+    var today = new Date();
+    var today_dd = String(today.getDate()).padStart(2, '0');
+    var today_mm = String(today.getMonth()).padStart(2, '0'); //January is 0!
+    var today_yyyy = today.getFullYear();
+    var today_string = new String(today_yyyy + '-' + today_mm + '-' + today_dd);
+    var today_final = today_string.split('-');
+
+    var today_format = new Date(today_final[0],today_final[1],today_final[2]);
+    var creation_format = new Date(creation_final[0],creation_final[1],creation_final[2]);
+
+    var diff = Math.abs(((today_format.getTime() - creation_format.getTime()) / (24 * 3600 * 1000)/365));
+
+    characterBirthdate.innerText = 'Created: ' + creation_final[2]+'/'+creation_final[1]+'/'+creation_final[0];
+
+    characterAge = document.createElement('span');
+    characterAge.className = "characterAge";
+    characterAge.innerText = 'Age: ' + Math.round(diff).toString().substring(0) + ' years, ' + diff.toString().split(".")[1].split("")[0] + ' months';
+
+    characterPlaytime = document.createElement('span');
+    characterPlaytime.className = "characterPlaytime";
+    let playtime = Math.floor(character_info.age/60/60);
+    characterPlaytime.innerText = `${playtime} hours played`;
 
     characterRace = document.createElement('a');
     characterRace.className = "characterRace";
-    characterRace.href = `https://wiki.guildwars2.com/wiki/${character.race}`;
-    characterRace.innerText = `${character.race}`;
-
-    // characterProf = document.createElement('a');
-    // characterProf.className = "characterProf";
-    // characterProf.href = `https://wiki.guildwars2.com/wiki/${character.profession}`;
-    // characterProf.innerText = `${character.profession}`;
+    characterRace.href = `https://wiki.guildwars2.com/wiki/${character_info.race}`;
+    characterRace.innerText = `${character_info.race}`;
 
     characterProf = document.createElement('a');
     characterProf.className = "characterProf";
-    characterProf.href = `https://wiki.guildwars2.com/wiki/${character.profession}`;
+    characterProf.href = `https://wiki.guildwars2.com/wiki/${elite_spec_name}`;
     characterProfPic = document.createElement('img');
     characterProfPic.className = "characterProfPic";
+
+    characterEliteSpec = document.createElement('a');
+    characterEliteSpec.className = "characterEliteProf";
+    characterEliteSpec.href = `https://wiki.guildwars2.com/wiki/${elite_spec_name}`;
+    characterEliteSpecPic = document.createElement('img');
+    characterEliteSpecPic.className = "characterEliteProf";
+
+    characterEliteSpec = document.createElement('span');
+    characterEliteSpec.className = "characterEliteProf";
+    characterEliteSpec.innerText = elite_spec_name;
+    
+
     //characterProfPic.src = `url('${get_profession_icon(character.profession)}')`;
     //characterProfPic.innerText = "url('https://render.guildwars2.com/file/A94D00911BD47CDE39A104F90C7D07DE623554ED/156631.png')";
     
     //characterProfPic.src = `${get_profession_icon(character.profession)}`;
     //characterProfPic.src = "https://render.guildwars2.com/file/A94D00911BD47CDE39A104F90C7D07DE623554ED/156631.png";
 
-    switch(character.profession) {
+    switch(character_info.profession) {
       case "Elementalist":
         characterBox.id = "Elementalist";
         profImage = "BBED46EB20C80D0DDE0F99402493C7E6FFAE1530/156629";
@@ -75,14 +135,36 @@ const get_character_info = (character_name) => {
         break;
       }
 
+    characterBox.id = elite_spec_name;
 
-      characterProfPic.src = `https://render.guildwars2.com/file/${profImage}.png`
-    //console.log(characterProfPic.src);
-    characterBox.append(characterName)
-    //characterBox.append(characterBirthdate)
-    characterProf.append(characterProfPic)
-    characterBox.append(characterProf)
-    characterBox.append(characterRace)
+    console.log(characterEliteSpec)
+
+    characterProfPic.src = `https://render.guildwars2.com/file/${profImage}.png`
+    characterEliteSpecPic.src = elite_spec_pic
+
+    header.append(characterName);
+
+    if (characterEliteSpecPic.src === "undefined") {
+      characterProf.append(characterProfPic);
+    }
+    else {
+      characterProf.append(characterEliteSpecPic);
+    }
+
+    aside_1.append(characterRace);
+    aside_1.append(characterEliteSpec);
+    aside_1.append(characterProf);
+    aside_2.append(characterBirthdate);
+    aside_2.append(document.createElement("br"));
+    aside_2.append(characterAge);
+    aside_2.append(document.createElement("br"));
+    aside_2.append(characterPlaytime);
+    footer.append('');
+    characterBox.append(header);
+    characterBox.append(aside_1);
+    characterBox.append(article);
+    characterBox.append(aside_2);
+    characterBox.append(footer);
     document.querySelector('#character_info').appendChild(characterBox);
   })
 };
@@ -93,20 +175,5 @@ const get_character_names = () => {
   })
   .then((character_list) => {
     character_list.sort().forEach(function(character) { get_character_info(character); });
-  })
-};
-
-const get_profession_icon = (profession_to_find) => {
-  fetch(`https://api.guildwars2.com/v2/professions/${profession_to_find}`).then((response) => {
-    return response.json();
-  })
-  .then((profession) => {
-    //console.log(profession.icon_big);
-    //return profession.icon_big;
-    //console.log(`url('${profession.icon_big}')`);
-    //return `url('${profession.icon_big}')`;
-    //console.log(`https://api.guildwars2.com/v2/professions/${profession_to_find}`)
-    //console.log(profession.icon_big);
-    return profession;
   })
 };
