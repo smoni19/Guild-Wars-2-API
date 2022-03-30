43f1e66229bd3ea467b16357de771e13883832cb
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-03-30 20:34:22 +0000

    Trait lines now displayed in correct order

--- public/get_character_build.js
+++ public/get_character_build.js
@@ -1,12 +1,15 @@
 const get_specialisation_info = (specialisation) => {
-  const build = document.getElementById('build');
-  build.innerHTML = "";
+
+  if (document.getElementById('loader')) {
+    document.getElementById('loader').remove();
+  }
+
   var selected_traits = specialisation.traits;
 
   fetch(`https://api.guildwars2.com/v2/specializations/${specialisation.id}`)
   .then((response) => {
     return response.json();
-      }).then((jsonData) => { 
+      }).then((jsonData) => {
         var specialisation_info = jsonData;
         var specialisation_id = specialisation_info.id;
         var specialisation_name = specialisation_info.name;
@@ -16,101 +19,101 @@ const get_specialisation_info = (specialisation) => {
         var specialisation_icon = specialisation_info.icon;
         var specialisation_background = specialisation_info.background;
 
-        const specialisation = document.createElement('div');
-        specialisation.className = "specialisation";
-        specialisation.style.backgroundImage = `url(${specialisation_background})`;
+        let spec_lines = document.getElementsByClassName('specialisation');
+        for (let spec_line of spec_lines) {
+          if (spec_line.id == specialisation_id) {
+            spec_line.style.backgroundImage = `url(${specialisation_background})`;
+            if (specialisation_elite == true) {
+              spec_line.classList.add('elite');
+            }
 
-        const specialisation_name_element = document.createElement('div');
-        specialisation_name_element.className = "specialisation_name";
-        specialisation_name_element.innerHTML = specialisation_name;
+            const specialisation_name_element = document.createElement('a');
+            specialisation_name_element.className = "specialisation_name";
+            specialisation_name_element.innerHTML = specialisation_name;
+            specialisation_name_element.href = `https://wiki.guildwars2.com/wiki/${specialisation_name}`
 
-        specialisation_major_traits.forEach(function(major_trait) {
-          fetch(`https://api.guildwars2.com/v2/traits/${major_trait}`)
-          .then((response) => {
-            return response.json();
-              }).then((jsonData) => {
-                var major_trait_info = jsonData;
-                var major_trait_id = major_trait_info.id;
-                var major_trait_tier = major_trait_info.tier;
-                var major_trait_order = major_trait_info.order;
-                var major_trait_name = major_trait_info.name;
-                var major_trait_description = major_trait_info.description;
-                var major_trait_slot = major_trait_info.slot;
-                var major_trait_facts = major_trait_info.facts;
-                var major_trait_speclisation = major_trait_info.specialization;
-                var major_trait_icon = major_trait_info.icon;
+            specialisation_major_traits.forEach(function(major_trait) {
+              fetch(`https://api.guildwars2.com/v2/traits/${major_trait}`)
+              .then((response) => {
+                return response.json();
+                  }).then((jsonData) => {
+                    var major_trait_info = jsonData;
+                    var major_trait_id = major_trait_info.id;
+                    var major_trait_tier = major_trait_info.tier;
+                    var major_trait_order = major_trait_info.order;
+                    var major_trait_name = major_trait_info.name;
+                    var major_trait_description = major_trait_info.description;
+                    var major_trait_slot = major_trait_info.slot;
+                    var major_trait_facts = major_trait_info.facts;
+                    var major_trait_speclisation = major_trait_info.specialization;
+                    var major_trait_icon = major_trait_info.icon;
 
-                var major_trait_icon_element = document.createElement('div');
-                var classNameString = `${major_trait_slot} tier-${major_trait_tier}-${major_trait_order}`
-                selected_traits.includes(major_trait_id) ? major_trait_icon_element.className = `selected ${classNameString}` : major_trait_icon_element.className = classNameString;
-                
-                major_trait_icon_element.id = `${major_trait_slot} ${major_trait_tier}`;
-                major_trait_icon_element.style.backgroundImage = `url(${major_trait_icon})`;
-                major_trait_icon_element.innerText = '';
+                    var major_trait_icon_element = document.createElement('div');
+                    var classNameString = `${major_trait_slot} tier-${major_trait_tier}-${major_trait_order}`
+                    selected_traits.includes(major_trait_id) ? major_trait_icon_element.className = `selected ${classNameString}` : major_trait_icon_element.className = classNameString;
 
-                trait_tooltip = document.createElement('span');
-                trait_tooltip.className = "tooltip";
-                if (major_trait_description != 'undefined') {
-                  trait_tooltip.innerText = `${major_trait_description}`;
-                }
-                major_trait_icon_element.append(trait_tooltip);
+                    major_trait_icon_element.id = `${major_trait_slot} ${major_trait_tier}`;
+                    major_trait_icon_element.style.backgroundImage = `url(${major_trait_icon})`;
+                    major_trait_icon_element.innerText = '';
 
-                var major_trait_link = document.createElement('a');
-                major_trait_link.href = `https://wiki.guildwars2.com/wiki/${major_trait_name}`;
-                major_trait_link.target = "_blank"
-                major_trait_link.rel = "noopener noreferrer"
-                major_trait_link.append(major_trait_icon_element)
+                    trait_tooltip = document.createElement('span');
+                    trait_tooltip.className = "tooltip";
+                    if (major_trait_description != 'undefined') {
+                      trait_tooltip.innerText = `${major_trait_description}`;
+                    }
+                    major_trait_icon_element.append(trait_tooltip);
 
-                specialisation.append(major_trait_link);
-              })
-            });
+                    var major_trait_link = document.createElement('a');
+                    major_trait_link.href = `https://wiki.guildwars2.com/wiki/${major_trait_name}`;
+                    major_trait_link.target = "_blank"
+                    major_trait_link.rel = "noopener noreferrer"
+                    major_trait_link.append(major_trait_icon_element)
 
-        specialisation_minor_traits.forEach(function(minor_trait) {
-          fetch(`https://api.guildwars2.com/v2/traits/${minor_trait}`)
-          .then((response) => {
-            return response.json();
-              }).then((jsonData) => {
-                var minor_trait_info = jsonData;
-                var minor_trait_id = minor_trait_info.id;
-                var minor_trait_tier = minor_trait_info.tier;
-                var minor_trait_order = minor_trait_info.order;
-                var minor_trait_name = minor_trait_info.name;
-                var minor_trait_description = minor_trait_info.description;
-                var minor_trait_slot = minor_trait_info.slot;
-                var minor_trait_facts = minor_trait_info.facts;
-                var minor_trait_speclisation = minor_trait_info.specialization;
-                var minor_trait_icon = minor_trait_info.icon;
-                
-                var minor_trait_icon_element = document.createElement('div');
+                    spec_line.append(major_trait_link);
+                  })
+                });
 
-                var classNameString = `${minor_trait_slot} tier-${minor_trait_tier}`
-                specialisation_minor_traits.includes(minor_trait_id) ? minor_trait_icon_element.className = `selected ${classNameString}` : minor_trait_icon_element.className = classNameString;
+            specialisation_minor_traits.forEach(function(minor_trait) {
+              fetch(`https://api.guildwars2.com/v2/traits/${minor_trait}`)
+              .then((response) => {
+                return response.json();
+                  }).then((jsonData) => {
+                    var minor_trait_info = jsonData;
+                    var minor_trait_id = minor_trait_info.id;
+                    var minor_trait_tier = minor_trait_info.tier;
+                    var minor_trait_order = minor_trait_info.order;
+                    var minor_trait_name = minor_trait_info.name;
+                    var minor_trait_description = minor_trait_info.description;
+                    var minor_trait_slot = minor_trait_info.slot;
+                    var minor_trait_facts = minor_trait_info.facts;
+                    var minor_trait_speclisation = minor_trait_info.specialization;
+                    var minor_trait_icon = minor_trait_info.icon;
 
-                minor_trait_icon_element.id = `${minor_trait_slot} ${minor_trait_tier}`;
-                minor_trait_icon_element.style.backgroundImage = `url(${minor_trait_icon})`;
-                minor_trait_icon_element.innerText = '';
+                    var minor_trait_icon_element = document.createElement('div');
 
-                trait_tooltip = document.createElement('span');
-                trait_tooltip.className = "tooltip";
-                if (minor_trait_description != null) {
-                  trait_tooltip.innerText = `${minor_trait_description}`;
-                }
-                else {
-                  trait_tooltip.innerText = `${minor_trait_facts}`;
-                }
+                    var classNameString = `${minor_trait_slot} tier-${minor_trait_tier}`
+                    specialisation_minor_traits.includes(minor_trait_id) ? minor_trait_icon_element.className = `selected ${classNameString}` : minor_trait_icon_element.className = classNameString;
 
-                minor_trait_icon_element.append(trait_tooltip);
+                    minor_trait_icon_element.id = `${minor_trait_slot} ${minor_trait_tier}`;
+                    minor_trait_icon_element.style.backgroundImage = `url(${minor_trait_icon})`;
+                    minor_trait_icon_element.innerText = '';
 
-                var minor_trait_link = document.createElement('a');
-                minor_trait_link.href = `https://wiki.guildwars2.com/wiki/${minor_trait_name}`;
-                minor_trait_link.target = "_blank"
-                minor_trait_link.rel = "noopener noreferrer"
-                minor_trait_link.append(minor_trait_icon_element);
-                specialisation.append(minor_trait_link);
-              })
-              specialisation.append(specialisation_name_element);
-              build.append(specialisation);
-            });
+                    trait_tooltip = document.createElement('span');
+                    trait_tooltip.className = "tooltip";
+
+                    minor_trait_description != null ? trait_tooltip.innerText = `${minor_trait_description}` : trait_tooltip.innerText = `${minor_trait_facts}`;
+                    minor_trait_icon_element.append(trait_tooltip);
+
+                    var minor_trait_link = document.createElement('a');
+                    minor_trait_link.href = `https://wiki.guildwars2.com/wiki/${minor_trait_name}`;
+                    minor_trait_link.target = "_blank"
+                    minor_trait_link.rel = "noopener noreferrer"
+                    minor_trait_link.append(minor_trait_icon_element);
+                    spec_line.append(minor_trait_link);
+                  })
+                  spec_line.append(specialisation_name_element);
+                });
+            }}
       })
 };
 
@@ -156,7 +159,7 @@ const get_specialisations = () => {
   build.innerHTML = '';
   //build.innerHTML = `Loading ${character}'s build...`;
   const loader = document.createElement('div');
-  loader.className = "loader"
+  loader.id = "loader"
   build.append(loader)
 
   //const name_uppercase = character.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
@@ -168,6 +171,10 @@ const get_specialisations = () => {
       specialisations = character.specializations.pve;
       traits = character.specializations.pve.traits;
       specialisations.forEach(function(specialisation) {
+        const spec = document.createElement('div');
+        spec.id = specialisation.id
+        spec.className = 'specialisation'
+        build.append(spec)
         get_specialisation_info(specialisation);
       });
   })
